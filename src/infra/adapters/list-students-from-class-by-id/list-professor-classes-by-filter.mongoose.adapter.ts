42a46168c45c5e0d131abff312c822816814bf88
Bruno <brunopp2@gmail.com>
2024-11-12 15:32:50 -0300

    alterado no controlador list-professor-classes-by-filter

--- src/infra/adapters/list-students-from-class-by-id/list-professor-classes-by-filter.mongoose.adapter.ts
+++ src/infra/adapters/list-students-from-class-by-id/list-professor-classes-by-filter.mongoose.adapter.ts
@@ -10,15 +10,27 @@ export class ListStudentsFromClassByIdMongooseAdapter implements ListStudentsFro
   constructor(private readonly StudentModel: Model<StudentDocument>) {}
 
   async execute({ classId }: ListStudentsFromClassByIdPortInput): Promise<ListStudentsFromClassByIdPortResult> {
-    const students = (await this.StudentModel.find<StudentDocument>({ classCodeList: { $in: [classId] } })
-      .lean()
-      .exec()) as StudentDocument[];
+    try {
+      const students = (await this.StudentModel.find<StudentDocument>({ classCodeList: { $in: [classId] } })
+        .lean()
+        .exec()) as StudentDocument[];
 
-    return this.mapStudentsToModel(students);
+      
+      console.log('studentDocumentList', students);
+
+      
+      return this.mapStudentsToModel(students);
+    } catch (error) {
+      console.error('Erro ao buscar os alunos:', error);
+      throw new Error('Falha ao buscar os alunos');
+    }
   }
 
   private mapStudentsToModel(studentDocumentList: StudentDocument[]): ListStudentsFromClassByIdPortResult {
-    console.log('studentDocumentList', studentDocumentList);
-    return [];
+    return studentDocumentList.map(student => ({
+      nome: student.name,  
+      ra: student.id,      
+      situacao: student.status,  
+    }));
   }
 }
